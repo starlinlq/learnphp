b45b1f73a25978a6bdcc0cb573080cdd80df0d1c
starlinlq <67881697+starlinlq@users.noreply.github.com>
2021-02-24 17:23:53 -0600

    pizza place

---
+++ LEARNPHP/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+// php boolean //
+// true = "1"
+// false = ""
+// is not equal !=
+// less or equal <=
+// greater or qual >=
+
+
+// strings //
+// 'shaun' < 'yoshi'; //first letter based on the alphabet is true;
+// uppercase letter is seen as less as an lowercase letter;
+
+//conditional statements //
+
+$price = 20;
+if ($price < 30) {
+    echo 'condition met';
+} else {
+    echo 'condition not met';
+}
+
+?>
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,40 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
 
-<?php
-// php boolean //
-// true = "1"
-// false = ""
-// is not equal !=
-// less or equal <=
-// greater or qual >=
+	include('config/db_connect.php');
 
+	// write query for all pizzas
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
-// strings //
-// 'shaun' < 'yoshi'; //first letter based on the alphabet is true;
-// uppercase letter is seen as less as an lowercase letter;
+	// get the result set (set of rows)
+	$result = mysqli_query($conn, $sql);
 
-//conditional statements //
+	// fetch the resulting rows as an array
+	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-$price = 20;
-if ($price < 30) {
-    echo 'condition met';
-} else {
-    echo 'condition not met';
-}
+	// free the $result from memory (good practise)
+	mysqli_free_result($result);
+
+	// close connection
+	mysqli_close($conn);
+
 
 ?>
 
+<!DOCTYPE html>
+<html>
+	
+	<?php include('templates/header.php'); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+	<h4 class="center grey-text">Pizzas!</h4>
 
-<body>
+	<div class="container">
+		<div class="row">
 
-</body>
+			<?php foreach($pizzas as $pizza): ?>
+
+				<div class="col s6 m4">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<ul class="grey-text">
+								<?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
+									<li><?php echo htmlspecialchars($ing); ?></li>
+								<?php endforeach; ?>
+							</ul>
+						</div>
+						<div class="card-action right-align">
+							<a class="brand-text" href="#">more info</a>
+						</div>
+					</div>
+				</div>
+
+			<?php endforeach; ?>
+
+		</div>
+	</div>
+
+	<?php include('templates/footer.php'); ?>
 
 </html>
